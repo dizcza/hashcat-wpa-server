cb9f80dab2e5f36de3638fb23b9175b2aa5b07ba
dizcza <dizcza@gmail.com>
2018-07-15 16:59:37 +0300

    cleaned bash files

--- bash/push_changes.sh
+++ bash/push_changes.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rsync -rave "ssh -i ${AWS_KEY}" --exclude-from=rsync.exclude . dizcza@${AWS_HOST}:/home/dizcza/hashcat-wpa-server
-rsync -ve "ssh -i ${AWS_KEY}" bash/docker_run.sh dizcza@${AWS_HOST}:/home/dizcza/hashcat-wpa-server
+rsync -rave "ssh -i ${AWS_KEY}" --exclude-from=rsync.exclude . `whoami`@${AWS_HOST}:/home/`whoami`/hashcat-wpa-server
+rsync -ve "ssh -i ${AWS_KEY}" bash/docker_run.sh `whoami`@${AWS_HOST}:/home/`whoami`/hashcat-wpa-server
