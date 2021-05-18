9fbf438dad77fddbb2e42667c31a9bfaf50c3d37
dizcza <dizcza@gmail.com>
2021-05-18 10:28:56 +0300

    accumulate essid password candidates into one file

--- app/attack/base_attack.py
+++ app/attack/base_attack.py
@@ -1,6 +1,4 @@
 import argparse
-import math
-import re
 import shutil
 import tempfile
 import time
@@ -14,15 +12,11 @@ from app.attack.convert import split_by_essid
 from app.attack.hashcat_cmd import HashcatCmdCapture, HashcatCmdStdout
 from app.config import ESSID_TRIED
 from app.domain import Rule, WordListDefault, Mask
-from app.logger import logger
 from app.utils import read_plain_key, subprocess_call, bssid_essid_from_22000, \
     check_file_22000
 from app.word_magic import create_digits_wordlist, create_fast_wordlists
-from app.word_magic.essid import collect_essid_parts, word_compounds, \
-    MAX_COMPOUNDS_DIGITS_APPEND
-from app.word_magic.hamming import hamming_ball
-from app.word_magic.wordlist import count_rules, count_wordlist, \
-    WORDLISTS_AVAILABLE
+from app.word_magic.essid import get_password_candidates
+from app.word_magic.wordlist import WORDLISTS_AVAILABLE
 
 
 def monitor_timer(func):
@@ -38,7 +32,7 @@ def monitor_timer(func):
     return wrapped
 
 
-def prepare_first_run():
+def download_wordlists():
     for wlist in WORDLISTS_AVAILABLE:
         wlist.download()
     create_digits_wordlist()
@@ -73,7 +67,6 @@ class BaseAttack:
         """
         ESSID_TRIED.parent.mkdir(parents=True, exist_ok=True)
         split_by_essid_dir = Path(tempfile.mkdtemp())
-        essid_as_wordlist_dir = Path(tempfile.mkdtemp())
 
         bssid_essid_tried = set()
         if ESSID_TRIED.exists():
@@ -87,89 +80,24 @@ class BaseAttack:
         else:
             files_split_by_essid = [self.file_22000]
 
-        for hcap_fpath_essid in tqdm(files_split_by_essid, desc="ESSID attack", disable=not self.verbose):
+        for hcap_fpath_essid in tqdm(files_split_by_essid, desc="ESSID attack",
+                                     disable=not self.verbose):
             bssid_essid = next(bssid_essid_from_22000(hcap_fpath_essid))
             if bssid_essid in bssid_essid_tried:
                 continue
             bssid, essid = bssid_essid.split(':')
             essid = bytes.fromhex(essid).decode('utf-8')
+            password_candidates = get_password_candidates(essid=essid)
 
-            # (1) Hamming ball attack
-            self._run_essid_hamming(hcap_fpath_essid=hcap_fpath_essid, essid=essid)
-
-            # (2) best64 rule attack
-            essid_filepath = essid_as_wordlist_dir / re.sub(r'\W+', '', essid)  # strip all except digits, letters and '_'
-            with open(essid_filepath, 'w') as f:
-                f.write('\n'.join(collect_essid_parts(essid)))
-            self._run_essid_rule(hcap_fpath=hcap_fpath_essid, essid_wordlist_path=essid_filepath)
-
-            # (3) digits_append attack
-            if len(word_compounds(essid)) > MAX_COMPOUNDS_DIGITS_APPEND:
-                with open(essid_filepath, 'w') as f:
-                    f.write('\n'.join(collect_essid_parts(essid, max_compounds=MAX_COMPOUNDS_DIGITS_APPEND)))
-            self._run_essid_digits(hcap_fpath_essid=hcap_fpath_essid, essid_wordlist_path=essid_filepath)
-
-            with open(ESSID_TRIED, 'a') as f:
-                f.write(bssid_essid + '\n')
-        shutil.rmtree(essid_as_wordlist_dir)
-        shutil.rmtree(split_by_essid_dir)
-
-    @staticmethod
-    def compute_essid_candidates_num(essid: str, hamming_d=2):
-        essid_parts = len(collect_essid_parts(essid))
-        n_rules = count_rules(Rule.ESSID)
-        n_compounds = essid_parts * n_rules
-        digits_append = count_wordlist(WordListDefault.DIGITS_APPEND.path)
-        if len(word_compounds(essid)) > MAX_COMPOUNDS_DIGITS_APPEND:
-            n_digits_append = len(collect_essid_parts(
-                essid, max_compounds=MAX_COMPOUNDS_DIGITS_APPEND)) * digits_append
-        else:
-            n_digits_append = essid_parts * digits_append
-        s = len(essid)
-        n_hamming = math.comb(s, hamming_d) * s ** hamming_d * 3 * 2
-        n_total = n_compounds + n_digits_append + n_hamming
-        # print(f"{n_total=:.2e}: {n_compounds=:.2e}, {n_digits_append=:.2e}, {n_hamming=:.2e}")
-        return n_total
-
-
-    @monitor_timer
-    def _run_essid_rule(self, hcap_fpath: Path, essid_wordlist_path: Path):
-        """
-        Run ESSID + best64.rule attack.
-        """
-        with tempfile.NamedTemporaryFile(mode='w') as f:
-            hashcat_stdout = HashcatCmdStdout(outfile=f.name)
-            hashcat_stdout.add_wordlists(essid_wordlist_path)
-            hashcat_stdout.add_rule(Rule.ESSID)
-            subprocess_call(hashcat_stdout.build())
-            hashcat_cmd = self.new_cmd(hcap_file=hcap_fpath)
-            hashcat_cmd.add_wordlists(f.name)
-            subprocess_call(hashcat_cmd.build())
-
-    @monitor_timer
-    def _run_essid_digits(self, hcap_fpath_essid: Path, essid_wordlist_path: str):
-        wordlist_order = [essid_wordlist_path, WordListDefault.DIGITS_APPEND.path]
-        for reverse in range(2):
-            with tempfile.NamedTemporaryFile(mode='w') as f:
-                hashcat_stdout = HashcatCmdStdout(outfile=f.name)
-                hashcat_stdout.add_wordlists(*wordlist_order, options=['-a1'])
-                subprocess_call(hashcat_stdout.build())
+            with tempfile.NamedTemporaryFile(mode='w+b') as f:
+                f.writelines(password_candidates)
                 hashcat_cmd = self.new_cmd(hcap_file=hcap_fpath_essid)
                 hashcat_cmd.add_wordlists(f.name)
                 subprocess_call(hashcat_cmd.build())
-            wordlist_order = wordlist_order[::-1]
 
-    @monitor_timer
-    def _run_essid_hamming(self, hcap_fpath_essid: Path, essid: str, hamming_dist_max=2):
-        essid_hamming = set()
-        essid_hamming.update(hamming_ball(s=essid, n=hamming_dist_max))
-        essid_hamming.update(hamming_ball(s=essid.lower(), n=hamming_dist_max))
-        logger.debug(f"Essid {essid} -> {len(essid_hamming)} hamming cousins with dist={hamming_dist_max}")
-        with tempfile.NamedTemporaryFile(mode='w') as f:
-            f.write('\n'.join(essid_hamming))
-            hashcat_cmd = self.new_cmd(hcap_file=hcap_fpath_essid)
-            hashcat_cmd.add_wordlists(f.name)
-            subprocess_call(hashcat_cmd.build())
+            with open(ESSID_TRIED, 'a') as f:
+                f.write(bssid_essid + '\n')
+        shutil.rmtree(split_by_essid_dir)
 
     @monitor_timer
     def run_digits8(self):
@@ -207,7 +135,7 @@ class BaseAttack:
 
     @monitor_timer
     def run_names(self):
-        with tempfile.NamedTemporaryFile(mode='w') as f:
+        with tempfile.NamedTemporaryFile(mode='w+b') as f:
             hashcat_stdout = HashcatCmdStdout(outfile=f.name)
             hashcat_stdout.add_wordlists(WordListDefault.NAMES_UA_RU)
             hashcat_stdout.add_rule(Rule.ESSID)
@@ -268,6 +196,5 @@ def crack_22000():
 
 
 if __name__ == '__main__':
-    # BaseAttack.compute_essid_candidates_num("lrtgn5s19b41e21f1202unc77i8093")
-    prepare_first_run()
+    download_wordlists()
     crack_22000()
--- app/word_magic/essid.py
+++ app/word_magic/essid.py
@@ -1,13 +1,24 @@
+import math
 import re
+import shutil
+import tempfile
 from itertools import permutations
+from pathlib import Path
 
 import wordninja
+
+from app.attack.hashcat_cmd import HashcatCmdStdout
+from app.domain import Rule, WordListDefault
+from app.logger import logger
+from app.utils import subprocess_call
+from app.word_magic.wordlist import count_wordlist, count_rules
+from app.word_magic.hamming import hamming_ball
 
 MAX_COMPOUNDS = 8  # max compounds for rule best64 attack
 MAX_COMPOUNDS_DIGITS_APPEND = 7  # max compounds for digits_append attack
 
 
-def split_uppercase(word: str) -> set:
+def _split_uppercase(word: str) -> set:
     """
     EverGreen -> Ever, Green
     """
@@ -19,15 +30,15 @@ def split_uppercase(word: str) -> set:
     return simple_words
 
 
-def word_compounds(word: str, min_length=2):
+def _word_compounds(word: str, min_length=2):
     return [compound for compound in wordninja.split(word) if len(compound) >= min_length]
 
 
-def word_compounds_permutation(word: str, max_compounds=MAX_COMPOUNDS, min_length=2, alpha_only=False):
+def _word_compounds_permutation(word: str, max_compounds=MAX_COMPOUNDS, min_length=2, alpha_only=False):
     """
     catonsofa -> cat, on, sofa
     """
-    compounds = word_compounds(word, min_length=min_length)
+    compounds = _word_compounds(word, min_length=min_length)
     if alpha_only:
         compounds = filter(re.compile("[a-z]", flags=re.IGNORECASE).match, compounds)
     compounds = sorted(compounds, key=len, reverse=True)[:max_compounds]
@@ -37,7 +48,7 @@ def word_compounds_permutation(word: str, max_compounds=MAX_COMPOUNDS, min_lengt
     return compounds_perm
 
 
-def collect_essid_parts(essid_origin: str, max_compounds=MAX_COMPOUNDS):
+def _collect_essid_parts(essid_origin: str, max_compounds=MAX_COMPOUNDS):
     def modify_case(word: str):
         return {word, word.lower(), word.upper(), word.capitalize(), word.lower().capitalize()}
 
@@ -45,16 +56,16 @@ def collect_essid_parts(essid_origin: str, max_compounds=MAX_COMPOUNDS):
     essid_parts = {essid_origin}
     essid_parts.add(re.sub(r'\W+', '', essid_origin))
     essid_parts.add(re.sub('[^a-z]+', '', essid_origin, flags=re.IGNORECASE))
-    essid_parts.update(word_compounds_permutation(essid_origin, max_compounds=max_compounds))
+    essid_parts.update(_word_compounds_permutation(essid_origin, max_compounds=max_compounds))
     regex_split_parts = regex_non_char.split(essid_origin)
     regex_split_parts = list(filter(len, regex_split_parts))
 
     for word in regex_split_parts:
-        essid_parts.update(word_compounds_permutation(word, max_compounds=max_compounds))
-        essid_parts.update(word_compounds_permutation(word.lower(), max_compounds=max_compounds))
+        essid_parts.update(_word_compounds_permutation(word, max_compounds=max_compounds))
+        essid_parts.update(_word_compounds_permutation(word.lower(), max_compounds=max_compounds))
 
     essid_parts.update(regex_split_parts)
-    essid_parts.update(split_uppercase(essid_origin))
+    essid_parts.update(_split_uppercase(essid_origin))
     for essid in list(essid_parts):
         essid = regex_non_char.sub('', essid)
         essid_parts.update(modify_case(essid))
@@ -64,9 +75,89 @@ def collect_essid_parts(essid_origin: str, max_compounds=MAX_COMPOUNDS):
     return essid_parts
 
 
+def _collect_essid_hamming(essid: str, hamming_dist_max=2):
+    essid_hamming = set()
+    essid_hamming.update(hamming_ball(s=essid, n=hamming_dist_max))
+    essid_hamming.update(hamming_ball(s=essid.lower(), n=hamming_dist_max))
+    logger.debug(f"Essid {essid} -> {len(essid_hamming)} hamming cousins with dist={hamming_dist_max}")
+    return essid_hamming
+
+
+def _collect_essid_rule(essid_wordlist_path: Path):
+    """
+    Run ESSID + best64.rule attack.
+    """
+    with tempfile.NamedTemporaryFile(mode='w+b') as f:
+        hashcat_stdout = HashcatCmdStdout(outfile=f.name)
+        hashcat_stdout.add_wordlists(essid_wordlist_path)
+        hashcat_stdout.add_rule(Rule.ESSID)
+        subprocess_call(hashcat_stdout.build())
+        with open(f.name) as f:
+            candidates = f.readlines()
+    return candidates
+
+
+def _collect_essid_digits(essid_wordlist_path: Path):
+    candidates = set()
+    wordlist_order = [essid_wordlist_path, WordListDefault.DIGITS_APPEND]
+    for reverse in range(2):
+        with tempfile.NamedTemporaryFile(mode='w+b') as ftemp:
+            hashcat_stdout = HashcatCmdStdout(outfile=ftemp.name)
+            hashcat_stdout.add_wordlists(*wordlist_order, options=['-a1'])
+            subprocess_call(hashcat_stdout.build())
+            with open(ftemp.name) as f:
+                candidates.update(f.readlines())
+        wordlist_order = wordlist_order[::-1]
+    return candidates
+
+
+def get_password_candidates_count(essid: str, hamming_d=2):
+    essid_parts = len(_collect_essid_parts(essid))
+    n_rules = count_rules(Rule.ESSID)
+    n_compounds = essid_parts * n_rules
+    digits_append = count_wordlist(WordListDefault.DIGITS_APPEND.path)
+    if len(_word_compounds(essid)) > MAX_COMPOUNDS_DIGITS_APPEND:
+        n_digits_append = len(_collect_essid_parts(
+            essid, max_compounds=MAX_COMPOUNDS_DIGITS_APPEND)) * digits_append
+    else:
+        n_digits_append = essid_parts * digits_append
+    s = len(essid)
+    n_hamming = math.comb(s, hamming_d) * s ** hamming_d * 3 * 2
+    n_total = n_compounds + n_digits_append + n_hamming
+    # print(f"{n_total=:.2e}: {n_compounds=:.2e}, {n_digits_append=:.2e}, {n_hamming=:.2e}")
+    return n_total
+
+
+def get_password_candidates(essid):
+    password_candidates = set()
+    essid_as_wordlist_dir = Path(tempfile.mkdtemp())
+
+    # (1) Hamming ball attack
+    password_candidates.update(_collect_essid_hamming(essid=essid))
+
+    # (2) best64 rule attack
+    # strip all except digits, letters and '_'
+    essid_filepath = essid_as_wordlist_dir / re.sub(r'\W+', '', essid)
+    with open(essid_filepath, 'w') as f:
+        f.writelines(_collect_essid_parts(essid))
+    password_candidates.update(_collect_essid_rule(essid_filepath))
+
+    # (3) digits_append attack
+    if len(_word_compounds(essid)) > MAX_COMPOUNDS_DIGITS_APPEND:
+        # Rewrite the file to limit the no. of compounds
+        with open(essid_filepath, 'w') as f:
+            f.writelines(_collect_essid_parts(essid,
+                                              max_compounds=MAX_COMPOUNDS_DIGITS_APPEND))
+    password_candidates.update(_collect_essid_digits(essid_filepath))
+
+    shutil.rmtree(essid_as_wordlist_dir)
+    return password_candidates
+
+
 if __name__ == '__main__':
+    # get_password_candidates_count("lrtgn5s19b41e21f1202unc77i8093")
     for essid in ["Tanya007", "My_rabbit", "Myrabbit", "MyRabbit", "PetitCafe2017"]:
-        compounds = sorted(word_compounds_permutation(essid))
-        candidates = sorted(collect_essid_parts(essid))
+        compounds = sorted(_word_compounds_permutation(essid))
+        candidates = sorted(_collect_essid_parts(essid))
         print(f"'{essid}'\n\t{len(compounds)} compounds: {compounds}")
         print(f"\t{len(candidates)} candidates: {candidates}")
